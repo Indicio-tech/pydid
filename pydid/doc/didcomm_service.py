d995ea8d27f9f22b5c38d4c084e31a46e8407a09
Daniel Bluhm <dbluhm@pm.me>
2021-03-15 15:42:12 -0400

    feat: add didcomm service type
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

---
+++ pydid/doc/didcomm_service.py
@@ -0,0 +1,67 @@
+"""Service type for DID Communication Services."""
+
+from typing import List
+
+from voluptuous import All, Any, PREVENT_EXTRA
+
+from ..did_url import DIDUrl
+from .service import Service
+
+
+class DIDCommService(Service):
+    """DID Communication Service."""
+
+    validate = Service.validate.extend(
+        {
+            "type": Any("IndyAgent", "did-communication"),
+            "recipientKeys": [All(str, DIDUrl.validate)],
+            "routingKeys": [All(str, DIDUrl.validate)],
+        },
+        extra=PREVENT_EXTRA,
+    )
+
+    def __init__(
+        self,
+        id_: DIDUrl,
+        endpoint: str,
+        recipient_keys: List[DIDUrl],
+        *,
+        type_: str = "did-communication",
+        routing_keys: List[DIDUrl] = None
+    ):
+        """Initialize DIDCommService."""
+        super().__init__(id_, type_, endpoint)
+        self._recipient_keys = recipient_keys
+        self._routing_keys = routing_keys or []
+
+    @property
+    def recipient_keys(self):
+        """Return recipient_keys."""
+        return self._recipient_keys
+
+    @property
+    def routing_keys(self):
+        """Return routing_keys."""
+        return self._routing_keys
+
+    def serialize(self):
+        """Return serialized representation of DIDCommService."""
+        return {
+            "id": self.id,
+            "type": self.type,
+            "serviceEndpoint": self.endpoint,
+            "recipientKeys": self.recipient_keys,
+            "routingKeys": self.routing_keys,
+        }
+
+    @classmethod
+    def deserialize(cls, value: dict):
+        """Deserialize into Service."""
+        value = cls.validate(value)
+        return cls(
+            id_=value["id"],
+            type_=value["type"],
+            endpoint=value["serviceEndpoint"],
+            recipient_keys=value["recipientKeys"],
+            routing_keys=value["routingKeys"],
+        )
