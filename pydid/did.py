d9d38e07e58cebbddefba4fbeecce1f3f5d4c378
Daniel Bluhm <dbluhm@pm.me>
2021-04-13 15:57:42 -0400

    feat: update did.py to play nicely with pydantic
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- pydid/did.py
+++ pydid/did.py
@@ -15,7 +15,7 @@ from .common import DID_PATTERN, DIDError
 from .did_url import DIDUrl
 
 
-class InvalidDIDError(DIDError, Invalid):
+class InvalidDIDError(DIDError, Invalid, ValueError):
     """Invalid DID."""
 
 
@@ -24,12 +24,22 @@ class DID:
 
     def __init__(self, did: str):
         """Validate and parse raw DID str."""
-        self._raw = did
         matched = DID_PATTERN.fullmatch(did)
         if not matched:
             raise InvalidDIDError("Unable to parse DID {}".format(did))
+        self._raw = did
         self._method = matched.group(1)
         self._id = matched.group(2)
+
+    @classmethod
+    def __get_validators__(cls):
+        """Yield validators for pydantic."""
+        yield cls._validate
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        """Update schema fields."""
+        field_schema.update(pattern=DID_PATTERN)
 
     @property
     def method(self):
@@ -81,3 +91,8 @@ class DID:
         if not cls.is_valid(did):
             raise InvalidDIDError('"{}" is not a valid DID'.format(did))
         return did
+
+    @classmethod
+    def _validate(cls, did):
+        """Pydantic validator."""
+        return cls(did)
