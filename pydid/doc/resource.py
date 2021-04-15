ef8ce93f36bc04e56d63d47b42cf2a2a1c281111
Daniel Bluhm <dbluhm@pm.me>
2021-04-14 23:45:26 -0400

    refactor: cleanup resource serialization
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- pydid/doc/resource.py
+++ pydid/doc/resource.py
@@ -5,8 +5,6 @@ from typing import Type, TypeVar
 from pydantic import BaseModel, Extra, parse_obj_as
 from inflection import camelize
 
-from ..did import DID
-from ..did_url import DIDUrl
 from ..validation import wrap_validation_error
 
 
@@ -21,7 +19,6 @@ class Resource(BaseModel):
 
         underscore_attrs_are_private = True
         extra = Extra.allow
-        json_encoders = {DID: str, DIDUrl: str}
 
         @classmethod
         def alias_generator(cls, string: str) -> str:
@@ -30,21 +27,7 @@ class Resource(BaseModel):
 
     def serialize(self):
         """Return serialized representation of Resource."""
-        result = {}
-        for key, value in self.dict(by_alias=True).items():
-            if key in self.__fields__:
-                type_ = self.__fields__[key].type_
-                if type_ in Resource.Config.json_encoders:
-                    value = Resource.Config.json_encoders[type_](value)
-
-            if isinstance(value, Resource):
-                result[key] = value.serialize()
-            elif isinstance(value, BaseModel):
-                result[key] = value.dict(by_alias=True)
-            else:
-                result[key] = value
-
-        return result
+        return self.dict(exclude_none=True, by_alias=True)
 
     @classmethod
     def deserialize(cls: Type[T], value: dict, *, type_: Type[T] = None) -> T:
