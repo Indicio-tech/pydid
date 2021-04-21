667a771e3cca6e77ad7b58319ded742ccbd0dbb1
sklump <srklump@hotmail.com>
2021-04-21 14:26:58 +0000

    kick out partial matches on DID parse
    Signed-off-by: sklump <srklump@hotmail.com>

--- pydid/common.py
+++ pydid/common.py
@@ -1,7 +1,7 @@
 """Common components."""
 import re
 
-DID_PATTERN = re.compile("did:([a-z0-9]+):((?:[a-zA-Z0-9._-]*:)*[a-zA-Z0-9._-]+)")
+DID_PATTERN = re.compile("^did:([a-z0-9]+):((?:[a-zA-Z0-9._-]*:)*[a-zA-Z0-9._-]+)$")
 
 
 class DIDError(Exception):
