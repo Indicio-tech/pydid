aef008f2ca81e5d917e07276202903c25b2d695c
Daniel Bluhm <dbluhm@pm.me>
2021-05-04 13:39:59 -0400

    feat: add make method for constructing resources
    This differs from init because it does not require literals to be
    present, automatically filling them in if not specified.
    
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- pydid/doc/resource.py
+++ pydid/doc/resource.py
@@ -1,9 +1,10 @@
 """Resource class that forms the base of all DID Document components."""
 
-from typing import Type, TypeVar
+from typing import Any, Dict, Type, TypeVar, get_args, get_origin
 
 from pydantic import BaseModel, Extra, parse_obj_as
 from inflection import camelize
+from typing_extensions import Literal
 
 from ..validation import wrap_validation_error
 
@@ -47,3 +48,30 @@ class Resource(BaseModel):
             message="Failed to deserialize {}".format(cls.__name__),
         ):
             return parse_obj_as(type_, value)
+
+    @classmethod
+    def _fill_in_required_literals(cls, **kwargs) -> Dict[str, Any]:
+        """Return dictionary of field name to value from literals."""
+        for field in cls.__fields__.values():
+            if (
+                field.required
+                and get_origin(field.type_) is Literal
+                and (field.name not in kwargs or kwargs[field.name] is None)
+            ):
+                kwargs[field.name] = get_args(field.type_)[0]
+        return kwargs
+
+    @classmethod
+    def _overwrite_none_with_defaults(cls, **kwargs) -> Dict[str, Any]:
+        """Overwrite none values in kwargs with defaults for corresponding field."""
+        for field in cls.__fields__.values():
+            if field.name in kwargs and kwargs[field.name] is None:
+                kwargs[field.name] = field.get_default()
+        return kwargs
+
+    @classmethod
+    def make(cls: Type[ResourceType], **kwargs) -> ResourceType:
+        """Create instance of class, filling in literals."""
+        kwargs = cls._fill_in_required_literals(**kwargs)
+        kwargs = cls._overwrite_none_with_defaults(**kwargs)
+        return cls(**kwargs)
