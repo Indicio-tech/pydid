42581bdd5ef2571dc38e8919129aa229ef01661d
Daniel Bluhm <dbluhm@pm.me>
2021-05-04 17:14:07 -0400

    feat: export BasicDIDDocument
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- pydid/__init__.py
+++ pydid/__init__.py
@@ -4,12 +4,13 @@ from .common import DIDError
 from .did import DID, InvalidDIDError
 from .did_url import DIDUrl, InvalidDIDUrlError
 from .doc import DIDDocumentError
-from .doc.doc import DIDDocument
+from .doc.doc import DIDDocument, BasicDIDDocument
 from .doc.builder import DIDDocumentBuilder
 from .doc.verification_method import VerificationMethod
 from .doc.service import Service, DIDCommService
 
 __all__ = [
+    "BasicDIDDocument",
     "DID",
     "DIDCommService",
     "DIDDocument",
