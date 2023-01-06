6e672a8b72a325cb36f656f8f4cd81e39ae5f710
Daniel Bluhm <dbluhm@pm.me>
2023-01-06 16:39:17 -0500

    docs: update examples in corrections
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- pydid/doc/corrections.py
+++ pydid/doc/corrections.py
@@ -19,7 +19,7 @@ def insert_missing_ids(value: dict) -> dict:
     >>> import pydid
     >>> doc_raw = {"@context": "http://example.com", "id": "did:example:123"}
     >>> doc = pydid.deserialize_document(
-    ...     doc, corrections=[pydid.corrections.insert_missing_ids]
+    ...     doc_raw, corrections=[pydid.corrections.insert_missing_ids]
     ... )
     """
     if "id" not in value:
@@ -58,15 +58,25 @@ def public_key_is_verification_method(value: dict) -> dict:
     with the pydid.deserialize_document method:
 
     >>> import pydid
-    >>> doc_raw = {"@context": "http://example.com", "id": "did:example:123"}
+    >>> doc_raw = {
+    ...     "@context": "http://example.com",
+    ...     "id": "did:example:123",
+    ...     "publicKey": [{"id": "did:example:123#key-1"}],
+    ... }
     >>> doc_raw = pydid.corrections.public_key_is_verification_method(doc_raw)
+    >>> assert "verificationMethod" in doc_raw
 
     Or:
     >>> import pydid
-    >>> doc_raw = {"@context": "http://example.com", "id": "did:example:123"}
+    >>> doc_raw = {
+    ...     "@context": "http://example.com",
+    ...     "id": "did:example:123",
+    ...     "publicKey": [{"id": "did:example:123#key-1"}],
+    ... }
     >>> doc = pydid.deserialize_document(
-    ...     doc, corrections=[pydid.corrections.public_key_is_verification_method]
+    ...     doc_raw, corrections=[pydid.corrections.public_key_is_verification_method]
     ... )
+    >>> assert doc.verification_method
     """
 
     if "publicKey" in value:
