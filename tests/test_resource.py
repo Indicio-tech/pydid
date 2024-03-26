001b079d083b6fd66622aa6f7ca04b3cd1da86f2
ff137 <ff137@proton.me>
2024-03-26 23:38:35 +0200

    :art: rename `mock_indexed_resource_factory` for clarity, and add typing to help the compiler

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -1,6 +1,6 @@
 """Test Resource."""
 
-from typing import Optional
+from typing import Callable, Generator, Optional, Type
 
 import pytest
 
@@ -13,8 +13,12 @@ from pydid.verification_method import (
 
 
 @pytest.fixture
-def mock_indexed_resource():
-    def _mock_indexed_resource(return_value=None):
+def mock_indexed_resource_factory() -> (
+    Generator[Callable[[Optional[Resource]], Type[IndexedResource]], None, None]
+):
+    """Factory fixture to create mock IndexedResource instances with customizable return values for dereference."""
+
+    def create_mock(return_value: Optional[Resource] = None) -> Type[IndexedResource]:
         class MockIndexedResource(IndexedResource):
             def _index_resources(self):
                 pass
@@ -24,7 +28,7 @@ def mock_indexed_resource():
 
         return MockIndexedResource()
 
-    yield _mock_indexed_resource
+    yield create_mock
 
 
 def test_resource_json_transforms():
@@ -35,7 +39,7 @@ def test_resource_json_transforms():
     assert Test.from_json('{"one": "test"}') == Test(one="test")
 
 
-def test_dereference_as_compatible(mock_indexed_resource):
+def test_dereference_as_compatible(mock_indexed_resource_factory):
     class One(Resource):
         common: str
         optional: Optional[str] = None
@@ -43,53 +47,53 @@ def test_dereference_as_compatible(mock_indexed_resource):
     class Two(Resource):
         common: str
 
-    test = mock_indexed_resource(One(common="common"))
+    test = mock_indexed_resource_factory(One(common="common"))
     assert isinstance(test.dereference_as(One, "test"), One)
     assert isinstance(test.dereference_as(Two, "test"), Two)
 
 
-def test_dereference_as_incompatible(mock_indexed_resource):
+def test_dereference_as_incompatible(mock_indexed_resource_factory):
     class One(Resource):
         one: str
 
     class Two(Resource):
         two: str
 
-    test = mock_indexed_resource(One(one="one"))
+    test = mock_indexed_resource_factory(One(one="one"))
     assert isinstance(test.dereference_as(One, "test"), One)
     with pytest.raises(ValueError):
         test.dereference_as(Two, "test")
 
 
-def test_dereference_as_vmethod(mock_indexed_resource):
+def test_dereference_as_vmethod(mock_indexed_resource_factory):
     resource = Resource(
         id="did:example:123#key-1",
         controller="did:example:123",
         type="Ed25519VerificationKey2018",
         public_key_base58="testing",
     )
-    test = mock_indexed_resource(resource)
+    test = mock_indexed_resource_factory(resource)
     result = test.dereference_as(VerificationMethod, "test")
     assert isinstance(result, VerificationMethod)
     assert result.public_key_base58 == "testing"
     assert result.material == "testing"
 
 
-def test_dereference_as_vmethod_x(mock_indexed_resource):
+def test_dereference_as_vmethod_x(mock_indexed_resource_factory):
     resource = Resource(type="agent", service_endpoint="http://example.com")
-    test = mock_indexed_resource(resource)
+    test = mock_indexed_resource_factory(resource)
     with pytest.raises(ValueError):
         test.dereference_as(VerificationMethod, "test")
 
 
-def test_dereference_as_vmethod_using_known_methods(mock_indexed_resource):
+def test_dereference_as_vmethod_using_known_methods(mock_indexed_resource_factory):
     resource = Resource(
         id="did:example:123#key-1",
         controller="did:example:123",
         type="Ed25519VerificationKey2018",
         public_key_base58="testing",
     )
-    test = mock_indexed_resource(resource)
+    test = mock_indexed_resource_factory(resource)
     result = test.dereference_as(KnownVerificationMethods, "test")
     assert isinstance(result, VerificationMethod)
     assert isinstance(result, Ed25519VerificationKey2018)
@@ -97,12 +101,12 @@ def test_dereference_as_vmethod_using_known_methods(mock_indexed_resource):
     assert result.material == "testing"
 
 
-def test_dereference_as_vmethod_using_known_methods_x(mock_indexed_resource):
+def test_dereference_as_vmethod_using_known_methods_x(mock_indexed_resource_factory):
     resource = Resource(
         id="did:example:123#did-communication",
         service_endpoint="did:example:123",
         type="did-communication",
     )
-    test = mock_indexed_resource(resource)
+    test = mock_indexed_resource_factory(resource)
     with pytest.raises(ValueError):
         test.dereference_as(KnownVerificationMethods, "test")
